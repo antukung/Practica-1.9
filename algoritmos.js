871d94eb6d685863a88ab285c043dd286e3de916
Antu <122485403+antukung@users.noreply.github.com>
2023-05-02 21:03:42 -0300

    ejercicio de programacion

---
+++ algoritmos.js
@@ -0,0 +1,8 @@
+cuerpoPagina= document.querySelector ("body");
+
+function boton() {
+    cuerpoPagina.style.background= "white";
+    cuerpoPagina.style.color= "black";
+    cuerpoPagina.style.fontFamily= "arial"    
+    cuerpoPagina.style.fontSize="35px";
+}
